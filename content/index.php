f011bf9074ad3cae1dc08b7b09e1003961b34b06
Adrian Smith <adrian.zymish@gmail.com>
2013-10-21 18:35:26 -0700

    Create filler content for homepage

--- content/index.php
+++ content/index.php
@@ -1,1 +1,8 @@
-<h4>Hi there.</h4>
+<div class='row'>
+	<div class='col-sm-3 hidden-phone'>
+		<img src='http://placehold.it/300&text=Generic+Image'>
+	</div>
+	<div class='col-sm-9'>
+		<p>There will be a bunch of text here. It may be an introduction, or maybe the most recent blog posts, or a slideshow of some kind. We'll see.</p>
+	</div>
+</div><!--.row-->
