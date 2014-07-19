8b245ee5c96de29d34c24f0c9c4684c95dcc2ea3
zymish <adrian.zymish@gmail.com>
2014-07-19 00:54:00 -0700

    Simplify theme

--- includes/config.php
+++ includes/config.php
@@ -1,4 +1,4 @@
 <?php
 define('GOOGLE_ANALYTIC','UA-5125769-21');
-define('SITE_ROOT','http://localhost/zymish/'); //Must end with trailing /
+define('SITE_ROOT','http://localhost/zymish/');
 define('DEFAULT_TITLE','ZyTest');
--- includes/startup.php
+++ includes/startup.php
@@ -3,17 +3,11 @@ flush();
 require_once('config.php');
 require_once('functions.php');
 $site = array('debug' => true);
-$projects=array(
-	1=>array(
-		'name'=>"The Palace (Working Title)",
-		'slug'=>'the-palace'
-	)
-);
 $site['css'] = array(
 	'bootstrap.min.css',
 	'zymish.css'
 );
 $site['js'] = array(
-	'excanvas.min.js',
-	'bootstrap.min.js'
+	'bootstrap.min.js',
+	'jquery.min.js'
 );
